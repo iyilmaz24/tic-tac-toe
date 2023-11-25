008f8a9e90cdb979d0ea21d3a9ced046e68b9632
Irfan Yilmaz <iy.usa@outlook.com>
2023-11-25 17:11:42 -0500

    begin implementing gameController, delete deprecated comments

--- javascript.js
+++ javascript.js
@@ -14,15 +14,18 @@ addEventListener("DOMContentLoaded", () => {
     for(let i = 0; i < 9; i++)
     {
         gridTiles[i].addEventListener("click", () => {
-            gameBoard.placeMarker(i);
+            gameController.makeMove(i);
         });
-        // write a callback function that validates tile is empty,
-            // places the piece, and then checks if game is won or not
-                // keep the functionality in gameBoard but call it in game controller
     }
 
-    // make a game controller that coordinates the flow of the game
-    // consider pros and cons of making it a IIFE
+    gameController = (function() {
+        const makeMove = (i) => {
+            gameBoard.placeMarker(i);
+            gameBoard.gameStatusCheck();
+        }
+
+        return {makeMove};
+    } ());
 
     gameBoard = (function() {
         const marker = "X";
@@ -51,11 +54,12 @@ addEventListener("DOMContentLoaded", () => {
             // make all tiles empty of markers, empty out boardArray
                 // give all click event listeners back to tiles
         }
-        const gameStatus = () => {
+        const gameStatusCheck = () => {
+            console.log("game status checker implemented here")
             // check game won or not through board array using an algo
         }
 
-        return {displayBoard, placeMarker};
+        return {displayBoard, placeMarker, gameStatusCheck};
 
     }());
 
