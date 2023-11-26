8f419bdc627815c8f4f625e1f3f21f66490fbcbf
Irfan Yilmaz <iy.usa@outlook.com>
2023-11-26 03:40:03 -0500

    expand gameController, implement 'Reset Board' button
    move global functions into gameController
    return former global functions as methods of gameController
    use gameController's new methods to set up the game
    implement the functionality for the 'resetBoard' function
    link 'resetBoard' function to the 'Reset Board' button
    delete deprecated comment and unused function

--- javascript.js
+++ javascript.js
@@ -1,22 +1,30 @@
 
 addEventListener("DOMContentLoaded", () => {
 
-    bigTitle = document.getElementById("big-title");
 
-    gameGrid = document.getElementById("game-grid");
-    gameGrid.innerHTML = "";
-    for(let i = 0; i < 9; i++) // use a for loop to populate grid with tiles
-    {
-        gameGrid.innerHTML += `<button class="tile" id="index-${i}" value="${i}"></button>`;
-    }
-
-    gridTiles = document.getElementsByClassName("tile");
-    for(let i = 0; i < 9; i++)
-    {
-        gridTiles[i].addEventListener("click", (e) => {gameController.makeMove(e.target.value)});
-    }
-
+    // use gameController to control flow of entire game
     gameController = (function() {
+
+        bigTitle = document.getElementById("big-title");
+        gameGrid = document.getElementById("game-grid");
+        gameGrid.innerHTML = "";
+
+        const populateTiles = () => {
+            for(let i = 0; i < 9; i++) // use a for loop to populate grid with tiles
+            {
+                gameGrid.innerHTML += `<button class="tile" id="index-${i}" value="${i}"></button>`;
+            }
+        }
+        const addClickListeners = () => {
+            gridTiles = document.getElementsByClassName("tile");
+            for(let i = 0; i < 9; i++) // add click event listener to each tile
+            {
+                gridTiles[i].addEventListener("click", (e) => {gameController.makeMove(e.target.value)});
+            }
+
+            resetButton = document.getElementById("reset-button");
+            resetButton.addEventListener("click", () => {gameBoard.resetBoard()});
+        }
         const makeMove = (i) => {
             if(gridTiles[i].innerHTML === "")
             {
@@ -31,19 +39,16 @@ addEventListener("DOMContentLoaded", () => {
             }
         }
 
-        return {makeMove};
+        return {makeMove, populateTiles, addClickListeners};
     } ());
 
 
-
+    // use gameBoard to manage the grid + game status, and place + track pieces
     gameBoard = (function() {
+
         const marker = "X";
-        const boardArray = [];
-        const displayBoard = () => {
-            for(let i = 0; i < 9; i++) {
-                console.log(boardArray[i]);
-            } 
-        }
+        let boardArray = [];
+
         const placeMarker = (position) => {
             if(gridTiles[position].innerHTML === "")
             {
@@ -51,24 +56,29 @@ addEventListener("DOMContentLoaded", () => {
                 gridTiles[position].innerHTML = marker;
             }
         }
-        
         const resetBoard = () => {
-// make all tiles empty of markers, empty out boardArray
+            boardArray = [];
+            for(let i = 0; i < 9; i++)
+            {
+                gridTiles[i].innerHTML = "";
+            }
         }
         const gameStatusCheck = () => {
             console.log("game status checker implemented here")
-// check game won or not through board array using an algo
+            // check game won or not through board array using an algo
         }
 
-        return {displayBoard, placeMarker, gameStatusCheck};
+        return {placeMarker, gameStatusCheck, resetBoard};
 
     }());
 
-// implement the functionality for the resetBoard function and link it to the 'Reset Board' button
 // implement 2nd player feature or randomized choice computer player
     // make game controller call the computer player to make a move after player
 // implement gameStatusCheck
     // use game controller to check the game status after a move and call resetBoard accordingly
     // implement a on-screen pop up or something telling user they won or lost through game controller
+
+    gameController.populateTiles();
+    gameController.addClickListeners();
 
 })
